8ee05d299732524e8c61ebd02ee9e7abfc435893
kbabi <kbabino@bk.ru>
2023-06-20 21:46:23 +0300

    design

--- app/src/main/java/com/example/a1/MainActivity.kt
+++ app/src/main/java/com/example/a1/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.a1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+    private var us
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+
+            }
 }
